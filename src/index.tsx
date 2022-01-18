22206951d69245bda6cad70178d6d3e17da096b3
badereddinkhmila <42270556+badereddinkhmila@users.noreply.github.com>
2022-01-18 22:22:07 +0100

    added configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
+/* istanbul ignore file */
 
-ReactDom.render(<App/>,document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
