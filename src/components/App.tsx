bda34ef6302288ecaecaa7a9feba13550d52610f
badereddinkhmila <42270556+badereddinkhmila@users.noreply.github.com>
2022-01-18 18:02:16 +0100

    scripts

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <>
+            <h3>
+                App component
+            </h3>
+        </>
+    )
+}
+export default App;
